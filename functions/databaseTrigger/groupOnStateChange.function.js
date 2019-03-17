d20ee73e657779091fed3f7f30e079c9321e58fc
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-17 16:39:58 +0100

    more setup

--- functions/databaseTrigger/groupOnStateChange.function.js
+++ functions/databaseTrigger/groupOnStateChange.function.js
@@ -1,24 +1,32 @@
-const functions = require('firebase-functions');
-const { db } = require('../initialize');
+const functions = require("firebase-functions");
+const { db } = require("../initialize");
 
-exports = module.exports = functions.database.ref('/users/{userId}/groups/{groupId}/current')
-  .onUpdate((change, context) => {
-    const before = change.before.val();
-    const after = change.after.val();
+exports = module.exports = functions.database
+	.ref("/users/{userId}/groups/{groupId}/current")
+	.onUpdate((change, context) => {
+		const before = change.before.val();
+		const after = change.after.val();
 
-    if (before === after) {
-      return null
-    }
+		if (before === after) {
+			return null;
+		}
 
-    const newState = after;
-    return db.ref(`/users/${context.params.userId}/groups/${context.params.groupId}/lamps`).once("value").then((lampsSnap) => {
-      let lampPromises = [];
-      if (lampsSnap.val()) {
-        const lamps = lampsSnap.val();
-        lampPromises = lamps.map((lampId) => {
-          return db.ref(`/users/${context.params.userId}/lamps/${lampId}/current`).set(newState);
-        });
-      }
-      return Promise.all(lampPromises);
-    });
-  });
+		const newState = after;
+		return db
+			.ref(
+				`/users/${context.params.userId}/groups/${context.params.groupId}/lamps`
+			)
+			.once("value")
+			.then((lampsSnap) => {
+				let lampPromises = [];
+				if (lampsSnap.val()) {
+					const lamps = lampsSnap.val();
+					lampPromises = lamps.map((lampId) => {
+						return db
+							.ref(`/users/${context.params.userId}/lamps/${lampId}/current`)
+							.set(newState);
+					});
+				}
+				return Promise.all(lampPromises);
+			});
+	});
--- functions/initialize.js
+++ functions/initialize.js
@@ -1,16 +1,16 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 try {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: "https://smartlight-4861d.firebaseio.com",
-  });
+	admin.initializeApp({
+		credential: admin.credential.applicationDefault(),
+		databaseURL: "https://smartlight-4861d.firebaseio.com",
+	});
 } catch (e) {
-  console.error(e)
+	console.error(e);
 }
 
 const db = admin.database();
 
 module.exports = {
-  db
-}
+	db,
+};
