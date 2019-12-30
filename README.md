bfd02a3895171fa9ff659c08de740c9b18861d47
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-12-30 22:05:06 +0100

    reduce deploy size

---
+++ README.md
@@ -0,0 +1,7 @@
+# SmartLight Firebase Functions
+
+## Setup
+
+1. install Yarn
+1. run `yarn setup`
+1. Start developing (`yarn serve`)
--- functions/populateGroupState.function.js
+++ functions/populateGroupState.function.js
@@ -1,10 +1,15 @@
 const functions = require("firebase-functions");
 const { db } = require("./initialize");
 
-function handleUnitChange(change, context) {
+// eslint-disable-next-line no-constant-condition
+const log = false ? console.log : () => {};
+
+function handleUnitChange(change) {
+	log("handleUnitChange invoked");
 	const unitBefore = change.before.data();
 	if (unitBefore.type !== "GROUP") {
 		// only changes on groups are handled by this function
+		log("but changed unit is not a group");
 		return false;
 	}
 	const unitAfter = change.after.data();
@@ -14,9 +19,11 @@ function handleUnitChange(change, context) {
 
 	if (JSON.stringify(stateBefore) === JSON.stringify(stateAfter)) {
 		// only state changes get processed
+		log("but unit state hasn't changed");
 		return false;
 	}
 
+	log("updating lamps...");
 	const lampUpdates = unitAfter.lamps.map((lampId) =>
 		db
 			.collection("units")
@@ -27,5 +34,5 @@ function handleUnitChange(change, context) {
 }
 
 exports = module.exports = functions.firestore
-	.document("users/{userId}")
+	.document("units/{unitId}")
 	.onUpdate(handleUnitChange);
